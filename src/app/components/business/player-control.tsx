0571f32b62dd656f5ea9dadfc1fd3e381c57343f
kakuqo <85939077@qq.com>
2024-03-16 18:54:25 +0800

    feat: 增加组件

---
+++ src/app/components/business/player-control.tsx
@@ -0,0 +1,40 @@
+import { TemoFileList } from "@/app/interface";
+import DataStore from "@/app/stores/dataStore";
+import { PlayerRef } from "@remotion/player";
+import { inject, observer } from "mobx-react";
+import { useEffect, useState } from "react";
+
+
+interface PlayerControlProps {
+    playerRef: PlayerRef
+    dataStore?: DataStore
+    fileList: TemoFileList[]
+}
+
+const PlayerControl = inject('dataStore')(observer(({ playerRef, dataStore, fileList }: PlayerControlProps) => {
+    const [list, setList] = useState<TemoFileList[]>()
+
+
+    useEffect(() => {
+        if (fileList.length) {
+            console.log(fileList)
+            setList(fileList)
+        }
+    }, [fileList])
+
+
+    return (
+        <div className="w-full h-24">
+            <div>
+                {list?.map((item: TemoFileList) => (
+                    <div className="border-r-2 border-gray-400 border-solid">
+                        <div className=" text-sm text-center flex-1">{item.text}</div>
+                        <div className=" flex-1"></div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}))
+
+export default PlayerControl
